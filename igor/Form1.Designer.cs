7d7dee6b88323a4fc058bcf72427f03c4a86f0cc
sheo888 <sasha1972md@gmail.com>
2020-11-04 16:30:56 +0300

    измена

--- igor/Form1.Designer.cs
+++ igor/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace igor
 {
-    partial class Form1
+    partial class Cupcha_
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -10,7 +10,7 @@
         /// <summary>
         /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложнja.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -28,13 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(23, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(304, 164);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(252, 191);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Далее";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 194);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(132, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Cupcha_
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(353, 228);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Cupcha_";
+            this.Text = "Капча";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- igor/Form1.cs
+++ igor/Form1.cs
@@ -10,11 +10,86 @@ using System.Windows.Forms;
 
 namespace igor
 {
-    public partial class Form1 : Form
+    public partial class Cupcha_ : Form
     {
-        public Form1()
+        public Cupcha_()
         {
             InitializeComponent();
+        }
+
+        private string text = String.Empty;
+
+        readonly Color red = Color.Red;
+
+        readonly Color black = Color.Black;
+
+        //private string text = String.Empty;
+
+        private Bitmap CreateImage(int Width, int Height)
+        {
+            Random rnd = new Random();
+
+            //Создадим изображение
+            Bitmap result = new Bitmap(Width, Height);
+
+            //Вычислим позицию текста
+            int Xpos = rnd.Next(0, Width - 50);
+            int Ypos = rnd.Next(15, Height - 15);
+
+            //Добавим различные цвета
+            Brush[] colors = { Brushes.Black,
+                     Brushes.Red,
+                     Brushes.RoyalBlue,
+                     Brushes.Green };
+
+            //Укажем где рисовать
+            Graphics g = Graphics.FromImage((Image)result);
+
+            //Сгенерируем текст
+            text = String.Empty;
+            string ALF = "1234567890QWERTYUIOPASDFGHJKLZXCVBNM";
+            for (int i = 0; i < 5; ++i)
+                text += ALF[rnd.Next(ALF.Length)];
+
+            //Нарисуем сгенирируемый текст
+            g.DrawString(text,
+                         new Font("Arial", 15),
+                         colors[rnd.Next(colors.Length)],
+                         new PointF(Xpos, Ypos));
+            
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox txtBox = ((TextBox)sender);
+            if (txtBox.Text == (String)txtBox.Tag)
+            {
+                txtBox.Text = String.Empty;
+                txtBox.ForeColor = black;
+                txtBox.Font = new Font("Microsoft Sans Serif", 12, FontStyle.Regular);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == this.text)
+            {
+                textBox1.Text = "";
+                    MessageBox.Show("правильно", "Ура", MessageBoxButtons.OK);
+
+            }
+            else
+            {
+                pictureBox1.Image = this.CreateImage(pictureBox1.Width, pictureBox1.Height);
+                MessageBox.Show("ошибка", "ERROR", MessageBoxButtons.OK);
+
+            }
         }
     }
 }
